2354ffa015525fe02ed5f20862d71d1742dcf2a6
lteyjolfur <lteyjolfur@gmail.com>
2024-12-25 19:33:37 +0100

    done

---
+++ src/solutions/25.ts
@@ -0,0 +1,79 @@
+import { readFileSync } from 'fs';
+
+const input = readFileSync('./input.txt','utf-8');
+
+
+
+const input_t =  `#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####
+`;
+
+type Int0to5 = -1| 0 | 1 | 2 | 3 | 4 | 5;
+
+type FiveT = Int0to5[]
+
+const locks:number[][] = [];
+const keys:number[][] = [];
+
+input.split('\n\n').forEach((string)=>{
+	const object = Array.from({length:5},()=>-1);
+	const isKey = string[0] === '.';
+	string.split('\n').forEach((row)=>row.split('').forEach(((c,i)=>{
+		object[i]+= Number(c==='#');
+	})) 
+	);
+	isKey?keys.push(object):locks.push(object);
+});
+
+let fits = 0;
+keys.forEach(key=>{
+	locks.forEach((lock)=>{
+		console.log(key,lock);
+		lock.every((t,i)=>{
+			const res = t+key[i]<6;
+			//console.log(key[i],t,res);
+			return res;
+		}) && fits++;
+	});
+});
+
+console.log(keys);
+console.log(locks);
+console.log(fits);
